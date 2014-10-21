7e15308a14236c475d7fc104f17f6fa3099d7235
Nico Kaag <nico.kaag@genj.nl>
2014-10-21 16:06:44 +0200

    Added basic bundle structure

---
+++ src/Genj/SsoServerBundle/GenjSsoServerBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Genj\SsoServerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class GenjSsoServerBundle
+ *
+ * @package Genj\GenjSsoServerBundle
+ */
+class GenjSsoServerBundle extends Bundle
+{
+}
